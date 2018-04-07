db82f62fd5ebac27298ca00133a3d332878c59a2
Adam Sanghera <adamsanghera@gmail.com>
2018-04-07 12:54:27 -0400

    unit tests and LL for conversations

--- user.go
+++ user.go
@@ -4,9 +4,11 @@ type User struct {
 	name string
 	uid  string
 
-	//
-	headConv *ConversationMeta // first element of LL
-	lastConv *ConversationMeta // last element of LL
+	// List of conversations
+	headConv       *ConversationMeta // first element of LL
+	lastConv       *ConversationMeta // last element of LL
+	convCacheSize  int               // current cache size
+	convCacheLimit int               // max size of LL
 }
 
 // ConversationMeta is a wrapper of Conversation
@@ -15,5 +17,55 @@ type ConversationMeta struct {
 	head *ConversationMeta
 	tail *ConversationMeta
 
-	CID string
+	CID int
+}
+
+func (u *User) UpdateConversationCache(CID int) error {
+
+	if u.convCacheSize == 0 {
+		u.headConv = &ConversationMeta{
+			head: nil,
+			tail: nil,
+			CID:  CID,
+		}
+
+		u.convCacheSize++
+
+		u.lastConv = u.headConv
+		return nil
+	}
+
+	if u.convCacheSize > u.convCacheLimit {
+		// Pop the end off
+		// Prepend new id
+		// Pop
+		penultimate := u.lastConv.head // second to last element
+		penultimate.tail = nil         // popping last element (making penultimate ultimate)
+		u.lastConv = penultimate       // Setting new last element
+
+		// Prepend
+		first := &ConversationMeta{ // make a new LL node
+			head: nil,
+			tail: u.headConv,
+			CID:  CID,
+		}
+		u.headConv.head = first // set head of old first to be new first
+		u.headConv = first      // set first to be first
+		return nil
+	}
+
+	// Prepend
+	first := u.headConv
+	first.head = &ConversationMeta{ // make a new LL node
+		head: nil,
+		tail: first,
+		CID:  CID,
+	}
+	u.headConv = first.head
+	// Prepend
+	// that's it
+
+	u.convCacheSize++
+
+	return nil
 }
